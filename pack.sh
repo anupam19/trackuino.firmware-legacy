49d2528b9547e66ba63dc72ed43f88e4178e8fda
jmartinj <jmartinj@localhost>
2010-04-10 03:10:57 +0000

    Fixed library 'make clean' not cleaning library deliverables; pack.sh to clean up before packing

--- pack.sh
+++ pack.sh
@@ -4,4 +4,6 @@
 export COPY_EXTENDED_ATTRIBUTES_DISABLE=true
 export COPYFILE_DISABLE=true
 #cd ..
+(cd src; make clean)
+(cd lib; make clean)
 tar -C .. -cvz --exclude=.* --exclude trackuino*tgz -f trackuino`date +%Y%m%d`.tgz trackuino
