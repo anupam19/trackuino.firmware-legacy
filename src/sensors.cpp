bb524645b05e8f36ab9b32bf333e2fc9a539b2f2
jmartinj <jmartinj@localhost>
2010-04-18 22:49:48 +0000

    Added preliminar sensors.cpp with functions to read the AREF voltage and the internal temperature sensor

---
+++ src/sensors.cpp
@@ -0,0 +1,70 @@
+/* trackuino copyright (C) 2010  EA5HAV Javi
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+/* Credit to:
+ *
+ * cathedrow for this idea on using the ADC as a volt meter:
+ * http://code.google.com/p/tinkerit/wiki/SecretVoltmeter
+ */
+
+#include "config.h"
+#include "sensors.h"
+#include <WProgram.h>
+
+/*
+ * sensors_aref: measure an external voltage hooked up to the AREF pin,
+ * optionally (and recommendably) through a pull-up resistor. This is
+ * incompatible with all other functions that use internal references
+ * (see config.h)
+ */
+#ifdef USE_AREF
+unsigned long sensors_aref()
+{
+  unsigned long result;
+  // Read 1.1V reference against AREF (p. 262)
+  ADMUX = _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
+  delay(2); // Wait for Vref to settle
+  ADCSRA |= _BV(ADSC); // Convert
+  while (bit_is_set(ADCSRA,ADSC));
+  result = (ADCH << 8) | ADCL;
+
+  // millivolts = 1.1 * 1024 * 1000 / result
+  result = 1126400 / result;
+
+  // aref = read aref * (32K + AREF_PULLUP) / 32K
+  result = result * (32000UL + AREF_PULLUP) / 32000;
+
+  return result;
+}
+#endif
+
+#ifndef USE_AREF
+long sensors_internal_temp()
+{
+  long result;
+  // Read temperature sensor against 1.1V reference
+  ADMUX = _BV(REFS1) | _BV(REFS0) | _BV(MUX3);
+  delay(2); // Wait for Vref to settle
+  ADCSRA |= _BV(ADSC); // Convert
+  while (bit_is_set(ADCSRA,ADSC));
+  result = (ADCH << 8) | ADCL;
+  
+  result = (result - 125) * 1075;
+
+  return result;
+}
+#endif
