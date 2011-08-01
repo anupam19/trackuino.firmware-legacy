6c7ce8f7b869d74644294b03ac0158273aaaf55b
jmartinj <jmartinj@localhost>
2011-08-01 22:32:32 +0000

    Branched 1.3

---
+++ trackuino/pack.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# abort on errors
+set -e
+
+# date
+date=`date +%Y%m%d`
+
+# version
+version=`cat VERSION`
+
+# avoid mac os copying ._* files
+export COPY_EXTENDED_ATTRIBUTES_DISABLE=true
+export COPYFILE_DISABLE=true
+
+# clean directory tree
+(cd src; make clean)
+(cd lib; make clean)
+rm -f eagle/*.b#? eagle/*.s#?
+rm -f eagle/shield/*.b#? eagle/shield/*.s#?
+rm -f eagle/trackuino-avr.{GBL,GBO,GBS,GTL,GTO,GTS,TXT,dri,gpi,GTP}
+
+# build arduino IDE package
+dst=tmp/trackuino-ide-$version
+mkdir -p $dst/trackuino
+cp src/*.{cpp,h} $dst/trackuino
+rm $dst/trackuino/main.cpp
+mv $dst/trackuino/trackuino.cpp $dst/trackuino/trackuino.pde
+mkdir $dst/eagle
+cp eagle/*.* $dst/eagle
+cp LICENSE $dst/LICENSE.txt
+cp README.ide-version  $dst/README.txt
+cp WHATSNEW $dst/WHATSNEW.txt
+for i in $dst/*.txt
+do
+  sed -i '' -e s/$// $i
+done
+test -f trackuino-ide-$version.zip && rm trackuino-ide-$version.zip
+(cd tmp; zip -9r ../trackuino-ide-$version.zip *) 
+rm -rf tmp
+
+# build makefile-based package for use with gcc-avr
+dst=tmp/trackuino-gcc-$version
+mkdir -p $dst
+cp -r WHATSNEW LICENSE include src Makefile.inc Makefile.user lib $dst
+mkdir $dst/eagle
+cp eagle/*.* $dst/eagle
+cp README.gcc-version $dst/README
+test -f trackuino-gcc-$version.tgz && rm trackuino-gcc-$version.tgz
+(cd tmp; tar -cvz --exclude=.* -f ../trackuino-gcc-$version.tgz *)
+rm -rf tmp
